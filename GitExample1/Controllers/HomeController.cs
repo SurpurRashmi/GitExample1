47599f7b9210387f0f73f94ae5515329aad45ae7
Rashmi R S <rashmirangana.surpu@hcl.com>
2024-09-10 15:42:25 +0530

    Add project files.

---
+++ GitExample1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using GitExample1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GitExample1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("This is Index-Master branch");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
